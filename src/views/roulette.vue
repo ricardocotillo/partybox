6b75bba495544b884bf1d83e1b0469d7992c8570
ricardocotillo <ricardo.cotillo@gmail.com>
2022-02-07 12:13:47 -0500

    fixed rotation

--- src/views/roulette.vue
+++ src/views/roulette.vue
@@ -35,7 +35,7 @@
           Girar
         </button>
       </div>
-      <img v-if="store.state.mode == 'hot'" class="w-full col-start-1 col-end-2 row-start-1 row-end-1" src="../assets/RULETA_HOT.svg" alt="ruleta" :style="{transform: `rotate(${angle}deg)`}" />
+      <img v-if="store.state.mode == 'hot'" class="w-full col-start-1 col-end-2 row-start-1 row-end-1" src="../assets/RULETA_HOT.svg" alt="ruleta" :style="{transform: `rotate(-${angle}deg)`}" />
       <img v-else class="w-full col-start-1 col-end-2 row-start-1 row-end-1" src="../assets/RULETA_TRANKI.svg" alt="ruleta" :style="{transform: `rotate(-${angle}deg)`}" />
       <transition
         enter-from-class="scale-0"
