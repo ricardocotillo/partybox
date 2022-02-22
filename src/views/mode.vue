0b4532d9df15dc0030ebd7edcc81f5da6710503f
ricardocotillo <ricardo.cotillo@gmail.com>
2022-02-22 11:56:36 -0500

    last fixes

--- src/views/mode.vue
+++ src/views/mode.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center">
     <img class="w-2/3 md:w-1/2 lg:w-1/4 mb-4" src="../assets/JUEGALO.svg" alt="juegalo" />
-    <img @click="click('hot')" v-show="!mode || mode == 'tranki'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/BOTON_INACTIVO_HOT.svg" alt="hot" />
-    <img @click="click('hot')" v-show="mode == 'hot'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/BOTON_ACTIVO_HOT.svg" alt="hot" />
+    <img @click="click('hot')" v-show="!mode || mode == 'tranki'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/mode/BOTON_INACTIVO_HOT.svg" alt="hot" />
+    <img @click="click('hot')" v-show="mode == 'hot'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/mode/BOTON_ACTIVO_HOT.svg" alt="hot" />
     <img class="w-1/12 md:w-10 lg:w-16 my-5" src="../assets/LETRA_O.svg" alt="letra o" />
-    <img @click="click('tranki')" v-show="!mode || mode == 'hot'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/BOTON_INACTIVO_TRANKI.svg" alt="tranki" />
-    <img @click="click('tranki')" v-show="mode == 'tranki'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/BOTON_ACTIVO_TRANKI.svg" alt="tranki" />
+    <img @click="click('tranki')" v-show="!mode || mode == 'hot'" class="w-1/2 md:w-1/6 cursor-pointer" :src="trankiInactivoSrc" alt="tranki" />
+    <img @click="click('tranki')" v-show="mode == 'tranki'" class="w-1/2 md:w-1/6 cursor-pointer" src="../assets/mode/BOTON_ACTIVO_TRANKI.svg" alt="tranki" />
   </div>
 </template>
 
 <script setup>
-import { inject, ref } from 'vue'
+import { computed, inject, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
@@ -23,6 +23,8 @@ const click = (m) => {
   mode.value = m
   setTimeout(() => router.push('roulette'), 500)
 }
+
+const trankiInactivoSrc = computed(() => new URL(`../assets/mode/${store.state.flavor}_tranki_inactivo.svg`, import.meta.url))
 </script>
 
 <style>
