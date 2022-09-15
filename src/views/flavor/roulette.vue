c57120e8a7090d3c620a255fbd0f9d2c6b9b572f
ricardocotillo <ricardo.cotillo@gmail.com>
2022-09-15 18:16:53 -0500

    transition

--- src/views/flavor/roulette.vue
+++ src/views/flavor/roulette.vue
@@ -2,10 +2,10 @@
   <div class="bg-black py-2">
     <img class="h-12 mx-auto" src="../../assets/lemondanger/logo-pb-danger.svg" alt="logo" />
   </div>
-  <div class="min-h-screen-content bg-cover bg-no-repeat bg-center pt-6" :style="{backgroundImage: 'url(/img/fondo.svg)'}">
-    <img class="w-60 mx-auto" src="../../assets/lemondanger/gira-la-ruleta.svg" alt="gira la ruleta" />
-    <img class="w-60 mx-auto" src="../../assets/lemondanger/gato-tranki.svg" alt="gato tranki" />
-    <div class="grid grid-cols-1 grid-rows-1 place-content-center place-items-center w-60 mx-auto -mt-5">
+  <div class="min-h-screen-content bg-cover bg-no-repeat bg-center pt-6 md:pt-20" :style="{backgroundImage: 'url(/img/fondo.svg)'}">
+    <img class="w-60 md:w-96 mx-auto" src="../../assets/lemondanger/gira-la-ruleta.svg" alt="gira la ruleta" />
+    <img class="w-60 md:w-96 mx-auto" src="../../assets/lemondanger/gato-tranki.svg" alt="gato tranki" />
+    <div class="grid grid-cols-1 grid-rows-1 place-content-center place-items-center w-60 mx-auto -mt-5 md:w-96 md:-mt-7">
       <img
         class="w-full col-start-1 row-start-1 col-span-1 row-span-1 transition-transform ease-roulette-out"
         src="../../assets/lemondanger/ruleta-de-castigos.svg"
@@ -14,7 +14,7 @@
         :style="{transform: `rotate(-${angle}deg)`}"
       />
       <img
-        class="w-20 col-start-1 row-start-1 col-span-1 row-span-1 z-10"
+        class="w-20 col-start-1 row-start-1 col-span-1 row-span-1 z-10 md:w-32"
         src="../../assets/lemondanger/girar.svg"
         alt="girar"
         @click="spin"
