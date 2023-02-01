2f848b0e45f1b016d074c078da3776b69f83c761
ricardocotillo <ricardo.cotillo@gmail.com>
2023-01-31 20:26:38 -0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -34,9 +34,26 @@ watch(flavor, newFlavor => {
 
 <template>
   <div
-    class="min-h-screen min-w-screen overflow-hidden relative"
+    class="relative min-h-screen overflow-hidden min-w-screen"
     :style= "bg"
   >
+    <template v-if="route.path.includes('promo')">
+      <a
+        target="_blank"
+        href="https://cms.partybox.com.pe/app/uploads/2023/02/Preguntas-Frecuentes.pdf"
+        download
+        class="fixed flex items-center justify-center text-3xl text-white rounded-full bottom-5 left-5 w-14 aspect-square"
+      >
+        <i class="lar la-question-circle"></i>
+      </a>
+      <a
+        target="_blank"
+        href="https://wa.me/51993920507"
+        class="fixed flex items-center justify-center text-3xl text-white bg-green-500 rounded-full bottom-5 right-5 w-14 aspect-square"
+      >
+        <i class="lab la-whatsapp"></i>
+      </a>
+    </template>
     <router-view />
   </div>
 </template>
