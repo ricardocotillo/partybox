9c59dba8019c919f2f17ea78374b60f7f10f0a9c
ricardocotillo <ricardo.cotillo@gmail.com>
2024-02-12 20:58:45 -0500

    fix

--- src/views/date.vue
+++ src/views/date.vue
@@ -8,8 +8,8 @@
     </div>
     <form @submit.prevent="validate" class="grid gap-2 grid-cols-3 mb-14 justify-center w-80 mx-auto">
       <input class="w-full placeholder:text-white py-6 px-5 font-extrabold text-center text-2xl uppercase bg-danger-negro text-white" placeholder="DD" @change="formatDay" type="text" v-maska data-maska="##" v-model="day" name="day">
-      <input class="w-full placeholder:text-white py-6 px-5 font-extrabold text-2xl uppercase bg-danger-negro text-white text-center" placeholder="MM" @change="formatMonth" type="text" name="month" v-mask data-maska="##" v-model="month">
-      <input class="w-full placeholder:text-white py-6 px-5 font-extrabold text-2xl uppercase bg-danger-negro text-white text-center" placeholder="AA" type="text" name="year" v-model="year">
+      <input class="w-full placeholder:text-white py-6 px-5 font-extrabold text-2xl uppercase bg-danger-negro text-white text-center" placeholder="MM" @change="formatMonth" type="text" name="month" v-maska data-maska="##" v-model="month">
+      <input class="w-full placeholder:text-white py-6 px-5 font-extrabold text-2xl uppercase bg-danger-negro text-white text-center" placeholder="AA" type="text" name="year" v-model="year" v-maska data-maska="##">
     </form>
     <img class="mx-auto mb-8" src="../assets/lata/bomb_and_cat.svg" alt="bomba y gato" />
     <img class="w-full mb-9" src="../assets/nivel_de_alcohol.svg" alt="nivel de alcohol">
@@ -18,10 +18,12 @@
 </template>
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { vMaska } from 'maska'
 const day = ref(null)
 const month = ref(null)
 const year = ref(null)
+const router = useRouter()
 
 const formatDay = e => {
   if (Number(e.target.value) <= 0) {
@@ -55,5 +57,7 @@ const validate = e => {
   const day = e.target.day.value
   const month = e.target.month.value
   const year = e.target.year.value
+  console.log('hereeee')
+  router.push({ name: 'links' })
 }
 </script>
