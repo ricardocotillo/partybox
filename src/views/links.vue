d2b77aa3c6248f838c55f996a206c4a858f19438
ricardocotillo <ricardo.cotillo@gmail.com>
2024-02-12 20:33:53 -0500

    fix

--- src/views/links.vue
+++ src/views/links.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="pt-16 bg-danger-amarillo w-full h-dvh overflow-y-auto">
-    <img class="mx-auto mb-11" src="../assets/la_fiesta.svg" alt="la fiesta a tu manera" />
+  <main class="py-8 bg-danger-amarillo w-full h-dvh overflow-y-auto">
+    <img class="mx-auto mb-11" src="../assets/lata/la_fiesta.svg" alt="la fiesta a tu manera" />
     <h3 class="font-extrabold uppercase mx-auto text-2xl w-82 text-center font-bricolage-grotesque mb-6">Elige cómo quieres empezar a divertirte</h3>
     <div class="flex flex-col gap-3 items-center">
       <a href="#" class="bg-danger-negro text-white font-extrabold uppercase text-2xl flex gap-3 justify-center py-6 px-5 w-70">
@@ -11,7 +11,7 @@
         Zona de juegos
       </router-link>
     </div>
-    <img class="mx-auto" src="../assets/jarra_bomba.svg" alt="jarra y bomba" />
+    <img class="mx-auto" src="../assets/lata/jarra_bomba.svg" alt="jarra y bomba" />
   </main>
 </template>
 <script>
