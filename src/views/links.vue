35b30e04f9becd43aa23de5b5489fc91434f0b20
ricardocotillo <ricardo.cotillo@gmail.com>
2024-12-20 12:09:45 -0500

    add page view event

--- src/views/links.vue
+++ src/views/links.vue
@@ -1,13 +1,13 @@
 <template>
-  <main class="py-8 bg-danger-amarillo w-full h-dvh overflow-y-auto">
+  <main class="w-full py-8 overflow-y-auto bg-danger-amarillo h-dvh">
     <img class="mx-auto mb-11" src="../assets/lata/la_fiesta.svg" alt="la fiesta a tu manera" />
-    <h3 class="font-extrabold uppercase mx-auto text-2xl w-82 text-center font-bricolage-grotesque mb-6">Elige cómo quieres empezar a divertirte</h3>
-    <div class="flex flex-col gap-3 items-center">
-      <a target="_blank" href="https://open.spotify.com/playlist/1loeQ4zSsEawYoaFmo5KAB?si=vphHST2BQKOPN0kbImScCA&pi=u-Zfy1deViSP2X" class="bg-danger-negro text-white font-extrabold uppercase text-2xl flex gap-3 justify-center py-6 px-5 w-70">
+    <h3 class="mx-auto mb-6 text-2xl font-extrabold text-center uppercase w-82 font-bricolage-grotesque">Elige cómo quieres empezar a divertirte</h3>
+    <div class="flex flex-col items-center gap-3">
+      <a target="_blank" href="https://open.spotify.com/playlist/1loeQ4zSsEawYoaFmo5KAB?si=vphHST2BQKOPN0kbImScCA&pi=u-Zfy1deViSP2X" class="flex justify-center gap-3 px-5 py-6 text-2xl font-extrabold text-white uppercase bg-danger-negro w-70">
         Playlist
         <img src="../assets/spotify.svg" alt="spotify logo" />
       </a>
-      <router-link to="/date/" class="bg-danger-negro text-white font-extrabold uppercase text-2xl flex gap-3 justify-center py-6 px-5 w-70">
+      <router-link to="/date/" class="flex justify-center gap-3 px-5 py-6 text-2xl font-extrabold text-white uppercase bg-danger-negro w-70">
         Zona de juegos
       </router-link>
     </div>
@@ -15,4 +15,11 @@
   </main>
 </template>
 <script setup>
+import { pageview } from 'vue-gtag'
+
+pageview({
+    page_title: 'Links',
+    page_location: 'https://partybox.com.pe/links',
+    page_path: '/links',
+  })
 </script>
